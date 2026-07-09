0b06ef495e0d50a31ca0be7ebe5a97746bc92cc3
Shivaprasad S Shetty <shivaprasadsshetty2004@gmail.com>
2026-07-09 14:50:39 +0530

    Created f3 file

---
+++ f3.java
@@ -0,0 +1,5 @@
+public class f3 {
+    public static void main(String[] args) {
+        System.out.println("F3 file is created");
+    }
+}
---
+++ f4.java
@@ -0,0 +1,5 @@
+public class f4 {
+    public static void main(String[] args) {
+        System.out.println("F4 file is created");
+    }
+}
